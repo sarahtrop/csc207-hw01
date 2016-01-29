94604885f31cd5169a4e67dc77dfe8ae1c420c3e
Sarah <sct14112@comcast.net>
2016-01-29 14:22:06 -0600

    Caeser Cipher additions

--- hw01/CaeserCipher/src/CaeserCipher.java
+++ hw01/CaeserCipher/src/CaeserCipher.java
@@ -1,12 +1,29 @@
 
 public class CaeserCipher {
 
-	/**
-	 * @param args
-	 */
+	//Encodes a message
+	public static void encode(String message) {
+		int base = (int) 'a';
+		
+		for (int i=0; i<26; i++) {
+			for (int j=0; j<message.length(); j++) {
+				int current = (((int)message.charAt(j)) - base);
+				int encryptedNum = (current + i) % 26;
+				char encryptedChar = (char) (encryptedNum + base);
+			}
+			
+		}
+	}
+	
+	//Decodes a message
+	public static void decode(String message) {
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		
 	}
 
 }
