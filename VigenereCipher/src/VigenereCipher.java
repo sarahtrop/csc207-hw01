a35fafdb8c4f283f6df0c14d818642f6dae11c93
Sarah <sct14112@comcast.net>
2016-01-30 13:31:15 -0600

    Encoding with the VigenereCipher

--- VigenereCipher/src/VigenereCipher.java
+++ VigenereCipher/src/VigenereCipher.java
@@ -1,9 +1,37 @@
 
 public class VigenereCipher {
 
-	/**
-	 * @param args
-	 */
+	public static void encode(String message, String keyword) {
+		int numLetters = message.length();
+		int base = (int)'a';
+		
+		//declaring and initialzing arrays to be used later
+		char[] messageCharArray = message.toCharArray();
+		char[] keywordCharArray = keyword.toCharArray();
+		
+		int[] messageIntArray = new int[numLetters];
+		int[] keywordIntArray = new int[numLetters];
+		
+		int[] encodedIntArray = new int[numLetters];
+		char[] encodedCharArray = new char[numLetters];
+		
+		//transposing characters in message and keyword to integers
+		for (int i=0; i<numLetters; i++) {
+			messageIntArray[i] = (int)messageCharArray[i] - base;
+			keywordIntArray[i] = (int)keywordCharArray[i] - base;
+		}
+		
+		//encoding message
+		for (int j=0; j<numLetters; j++) {
+			
+		}
+		
+	}
+	
+	public static void decode(String message, String keyword) {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
