569ccf27c089053d3cdb1db94cbfe33d279e2d7a
Sarah Trop <sctrop@gmail.com>
2016-02-02 14:53:27 -0600

    Finished Vigenere Cipher

--- CaeserCipher/src/CaeserCipher.java
+++ CaeserCipher/src/CaeserCipher.java
@@ -4,65 +4,80 @@ public class CaeserCipher {
 
 	public static int[] convertToInts(char[] chArray) {
 		int[] intArray = new int[chArray.length];
-		int base = (int)'a';
-		
-		for (int i=0; i<chArray.length; i++) {
-			intArray[i] = (int)chArray[i] - base;
+		int base = (int) 'a';
+
+		for (int i = 0; i < chArray.length; i++) {
+			intArray[i] = (int) chArray[i] - base;
 		}
 		return intArray;
 	}
-	
-	public static char[] encryptOrDecryptChars(char[] charArray, String cryptDir) {
-		int[] intArray = convertToInts(charArray.toCharArray());
-		int[] newIntArray = new int[charArray.length];
+
+	public static char[] encryptOrDecryptChars(char[] charArray, String cryptDir, int itr) {
+		int[] intArray = convertToInts(charArray);
 		char[] newCharArray = new char[charArray.length];
-		int base = (int)'a';
-		
-		for (int j=1; j<26; j++) {
-			for (int k=0; k<message.length(); k++) {
-				if (cryptDir.equals("encrypt")) { int num = (newIntArray[k] + j) % 26; }
-				else if (cryptDir.equals("decrypt")) { int num = (newIntArray[k] - j) % 26; }
-				newCharArray[k] = (char)(num + base);
-				}
+		int base = (int) 'a';
+		int num = 0;
+
+		for (int k = 0; k < intArray.length; k++) {
+			if (cryptDir.equals("encrypt")) {
+				num = (intArray[k] + itr) % 26;
+			} else if (cryptDir.equals("decrypt")) {
+				num = (intArray[k] - itr) % 26;
 			}
+			newCharArray[k] = (char) (num + base);
 		}
 		return newCharArray;
 	}
-	
+
+	public static void printIteration (String str, int itr) {
+		System.out.println("n = " + itr + ": " + str);
+	}
 	
 	public static void encrypt(String message) {
 		System.out.println("n = 0: " + message);
-		char[] encryptedCharArray = encryptOrDecryptChars(message.toCharArray, "encrypt");
-		String encryptedString = new String(encryptedCharArray);
-		System.out.println("n = " + i + ": " + encryptedString);
-	}
-	
-	public static void decrypt(String message) {
-		System.out.println("n = 0: " + message);
-		char[] decryptedCharArray = encryptOrDecryptChars(message.toCharArray, "decrypt");
-		String decryptedString = new String(decryptedCharArray);
 		
-		String decryptedString = new String(decryptedCharArray);
-		System.out.println("n = " + i + ": " + decryptedString);
+		for (int j = 1; j <26; j++) {
+			char[] encryptedCharArray = encryptOrDecryptChars(message.toCharArray(),
+					"encrypt", j);
+			String encryptedMessage = "" + encryptedCharArray[0];
+			if (message.length() > 1) {
+				for (int i=1; i<message.length(); i++) {
+					encryptedMessage += encryptedCharArray[i];
+				}
+			}
+			printIteration(encryptedMessage, j);
 		}
 	}
-	
-	public static void main(String[] args) {
+
+	public static void decrypt(String message) {
+		System.out.println("n = 0: " + message);
 		
-		System.out.println("This program encrypts and decrypts messages using the Caeser Cipher.\n Would you like to enccrypt or decrypt a message?");
+		for (int j = 1; j<26; j++) {
+			char[] decryptedCharArray = encryptOrDecryptChars(message.toCharArray(),
+					"decrypt", j);
+			String decryptedMessage = "" + decryptedCharArray[0];
+			if (message.length() > 1) {
+				for (int i=1; i<message.length(); i++) {
+					decryptedMessage += decryptedCharArray[i];
+				}
+			}
+			printIteration(decryptedMessage, j);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		System.out.print("This program encrypts and decrypts messages using the Caeser Cipher.\nWould you like to encrypt or decrypt a message? ");
 		Scanner in = new Scanner(System.in);
 		String choice = in.nextLine();
-		
-		if (choice.equals("encrypt")) { 
-			System.out.print("Enter the string to encrypt:"); 
-			String message = in.nextLine();
+		System.out.print("Enter the string to " + choice + ": ");
+		String message = in.nextLine();
+
+		if (choice.equals("encrypt")) {
 			encrypt(message);
-			}
-		else if (choice.equals("decrypt")) { 
-			System.out.print("Enter the string to decrypt:"); 
-			String message = in.nextLine();
+		} else if (choice.equals("decrypt")) {
 			decrypt(message);
-			}
+		}
 		in.close();
 	}
 
--- VigenereCipher/src/VigenereCipher.java
+++ VigenereCipher/src/VigenereCipher.java
@@ -4,78 +4,88 @@ public class VigenereCipher {
 
 	public static int[] convertToInts(char[] chArray) {
 		int[] intArray = new int[chArray.length];
-		int base = (int)'a';
-		
-		for (int i=0; i<chArray.length; i++) {
+		int base = (int) 'a';
+
+		for (int i = 0; i < chArray.length; i++) {
 			intArray[i] = (int)chArray[i] - base;
 		}
 		return intArray;
 	}
-	
+
 	public static char[] wrapKey(String keyword, int length) {
 		char[] keywordCharArray = new char[length];
-		for (int l=0; l<length; l++) {
+		for (int l = 0; l < length; l++) {
 			keywordCharArray[l] = keyword.charAt(l % keyword.length());
 		}
 		return keywordCharArray;
 	}
-	
+
 	public static char encryptOrDecryptChar(int ch, int keyCh, String cryptDir) {
-		int base = (int)'a';
-		
-		if (cryptDir.equals("encrypt")) { int num = ch + keyCh; }
-		else if (cryptDir.equals("decrypt")) { int num = ch - keyCh; }
-		
-		char finalChar = (char)(num + base);
+		int base = (int) 'a';
+		int num = 0;
+
+		if (cryptDir.equals("encrypt")) {
+			num = (ch + keyCh) % 26;
+		} else if (cryptDir.equals("decrypt")) {
+			num = (ch - keyCh) % 26;
+		}
+		char finalChar = (char) (num + base);
 		return finalChar;
 	}
-	
+
 	public static void encrypt(String message, String key) {
 		char[] keyCharArray = wrapKey(key, message.length());
 		int[] messageIntArray = convertToInts(message.toCharArray());
 		int[] keyIntArray = convertToInts(keyCharArray);
 		char[] encryptedCharArray = new char[message.length()];
-		
-		for (int j=0; j<message.length(); j++) {
-			encryptedCharArray[j] = encryptOrDecryptChar(messageIntArray[j], keyIntArray[j], "encrypt");
+
+		for (int j = 0; j < message.length(); j++) {
+			encryptedCharArray[j] = encryptOrDecryptChar(messageIntArray[j],
+					keyIntArray[j], "encrypt");
 		}
 		
-		String encryptedMessage = encryptedCharArray.toString();
+		String encryptedMessage = "" + encryptedCharArray[0];
+		
+			if (message.length() > 1) {
+				for (int i=1; i<message.length(); i++) {
+					encryptedMessage += encryptedCharArray[i];
+				}
+			}
 		System.out.println(encryptedMessage);
 	}
-	
+
 	public static void decrypt(String message, String key) {
 		char[] keyCharArray = wrapKey(key, message.length());
 		int[] messageIntArray = convertToInts(message.toCharArray());
 		int[] keyIntArray = convertToInts(keyCharArray);
 		char[] decryptedCharArray = new char[message.length()];
-		
-		for (int j=0; j<message.length(); j++) {
-			decryptedCharArray[j] = encryptOrDecryptChar(messageIntArray[j], keyIntArray[j], "decrypt");
+
+		for (int j = 0; j < message.length(); j++) {
+			decryptedCharArray[j] = encryptOrDecryptChar(messageIntArray[j],
+					keyIntArray[j], "decrypt");
 		}
-		
+
 		String decryptedMessage = new String(decryptedCharArray);
 		System.out.println(decryptedMessage);
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println("This program encrypts and decrypts messages using the Vigenere Cipher.\nWould you like to encode or decode a message?");
+		System.out.print("This program encrypts and decrypts messages using the Vigenere Cipher.\nWould you like to encrypt or decrypt a message? ");
 		Scanner in = new Scanner(System.in);
 		String choice = in.nextLine();
 
-		
-		if (choice.equals("encrypt")) { System.out.print("Enter the string to encrypt:"); }
-		else if (choice.equals("decrypt")) { System.out.print("Enter the string to decrypt:"); }
+		System.out.print("Enter the string to " + choice + ": ");
 		String message = in.nextLine();
-		
-		System.out.print("Enter the key:");
-		String key = in.nextLine();
-		
-		if (choice.equals("encrypt")) { encrypt(message, key); }
-		else if (choice.equals("decrypt")) { decrypt(message, key); }
-		
-		in.close();
-		}
-	}
 
+		System.out.print("Enter the key: ");
+		String key = in.nextLine();
+
+		if (choice.equals("encrypt")) {
+			encrypt(message, key);
+		} else if (choice.equals("decrypt")) {
+			decrypt(message, key);
+		}
+
+		in.close();
+	}
 }
